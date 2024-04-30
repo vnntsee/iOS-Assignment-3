34f1dd49bf5d4cd0903a85d6a645a77f77c54a21
vnntsee <VannesaLawren.The@student.uts.edu.au>
2024-04-30 11:13:36 +1000

    Creating Project

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Vannesa The on 30/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
