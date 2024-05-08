dc92a46056624a48c5bbe084524e9b43818c7028
CatKatCat <163791452+CatKatCat@users.noreply.github.com>
2024-05-08 17:04:27 +1000

    Add select priority

--- HabitTracker/HabitTracker/Views/AddHabitsView.swift
+++ HabitTracker/HabitTracker/Views/AddHabitsView.swift
@@ -11,6 +11,8 @@ struct AddHabitsView: View {
     
     @State var newHabitName = ""
     
+    @State var newPriority: Int = 2
+    
     var body: some View {
         ZStack {
             Color(UIColor(named: "LightYellow") ?? UIColor(Color.yellow.opacity(0.4)))
@@ -21,14 +23,38 @@ struct AddHabitsView: View {
                     .font(.largeTitle)
                     .foregroundStyle(.orange)
                     .bold()
-                    .padding()
+                    .padding(20)
                 
-                TextField("Enter Habit...", text: $newHabitName)
+                Text("Enter the name of your habit:")
+                    .font(.title3)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                
+                TextField("e.g. drink water once every hour.", text: $newHabitName)
                     .padding()
                     .frame(maxWidth: .infinity, maxHeight: 55)
                     .background(.regularMaterial)
                     .cornerRadius(15)
-                    .padding()
+                    .padding(.horizontal)
+                    .padding(.bottom)
+                
+                Text("Select your habit's priority:")
+                    .font(.title3)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    .padding(.top)
+                
+                Picker(selection: $newPriority, label: Text("Picker")) {
+                    Text("Low").tag(1)
+                    Text("Medium").tag(2)
+                    Text("High").tag(3)
+                }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
+                
                 Spacer()
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                     Text("Add Habit")
